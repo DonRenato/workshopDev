2c648b9f40823546c949135d1ddfa09c0eb16b53
0n1zuk4 <ravellar.machado@outlook.com>
2020-04-01 18:25:58 -0300

    pagina ideias

--- scripts.js
+++ scripts.js
@@ -3,6 +3,21 @@ function onOf(){
         .querySelector("#modal")
         .classList
         .toggle("hide")
+
+
+    document
+        .querySelector("body")
+        .classList
+        .toggle("hideScroll")
+
+    document
+        .querySelector("#modal")
+        .classList
+        .toggle("addScroll")
+
+            
+        
+
 }
 
 
